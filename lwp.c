0d2d5b24a418147c0aa59dcb7315b5f4829f9252
Dusen <god.88@live.com>
2024-05-11 17:45:46 +0800

    thread add atomic opration

--- lwp.c
+++ lwp.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdatomic.h>
 
 #define LOG_S(x)  printf("%s:: %s\n", __func__, x)
 #define LOG_I(x)  printf("%s:: %s = %d\n", __func__, #x, x)
@@ -38,14 +39,39 @@ void* f_mutex_2(void* arg)
     }
 }
 
+
+#define AUTOMIC_LOOP_TIME 111111
+int g_No_Atomic = 0;
+//atomic_t g_Atomic = ATOMIC_INIT(0);     // 用于内核开发
+atomic_int g_Atomic = ATOMIC_VAR_INIT(0); // 是C11标准, 在C17弃用,在C23中已经删除
+
+void* f_automic_1(void* arg)
+{
+    for (int i = 0; i < AUTOMIC_LOOP_TIME; i++) {
+        ++g_No_Atomic;
+        atomic_fetch_add(&g_Atomic, 1);
+    }
+    return NULL;
+}
+void* f_automic_2(void* arg)
+{
+    for (int i = 0; i < AUTOMIC_LOOP_TIME; i++) {
+        ++g_No_Atomic;
+        atomic_fetch_add(&g_Atomic, 1);
+    }
+    return NULL;
+}
+
 int main(int argc, char** argv)
 {
     int rt = 0;
-    int i = 0, pid = 0, ppid = 0;
+    int i = 0, pid = 0;
 
     void*(*f_list[])(void*) = {
         f_mutex_1,
-        f_mutex_2
+        f_mutex_2,
+        f_automic_1,
+        f_automic_2,
     };
     pthread_t tid[255] = {0};
 
@@ -60,6 +86,9 @@ int main(int argc, char** argv)
     }
 
     pthread_mutex_destroy(&mutex);
+    int l_atomic = atomic_load(&g_Atomic);
+    LOG_I(g_No_Atomic); // 必然小于 AUTOMIC_LOOP_TIMEx2
+    LOG_I(l_atomic);
 
     LOG_I(rt);
     return rt;
